5eac8440568637f59739f2e599504dc581cabc40
Joon SeSAC <twszak01@gmail.com>
2023-12-07 19:08:39 +0900

    모델 작업 완료

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'board.apps.BoardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
